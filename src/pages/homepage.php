413110f975e65525503cd43402ac0e6cd0278998
Katie Elizabeth <32232155+pixiekat@users.noreply.github.com>
2024-07-20 01:52:47 -0400

    doctrine and other libraries

--- src/pages/homepage.php
+++ src/pages/homepage.php
@@ -1,1 +1,23 @@
-<?php echo $twig->render('pages\homepage.html.twig', []); ?>
+<?php
+namespace App\pages;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
+
+/*
+$email = (new Email())
+    ->from('katie@pixiekitten.net')
+    ->to('calamitousgrace@gmail.com')
+    ->subject('Your Subject')
+    ->text('Hello, this is the plain text content.')
+    ->html('<p>Hello, this is the HTML content.</p>');
+
+// You can add more headers or attachments as needed
+// $email->attachFromPath('/path/to/attachment.pdf');
+
+// Send the email
+$transport = Transport::fromDsn($request->server->get('MAILER_DSN'));
+$mailer = new Mailer($transport);
+$mailer->send($email);*/
+
+echo $app->getTwig()->render('pages\homepage.html.twig', []);
